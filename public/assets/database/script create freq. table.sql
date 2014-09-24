552f6a4ffe3bd9ce3afa29e016cd9e7d9e3d1208
brucou <bruno.couriol@gmail.com>
2014-09-24 20:01:11 +0200

    add the files to fill in with data the heroku database and updated the creational scripts

--- public/assets/database/script create freq. table.sql
+++ public/assets/database/script create freq. table.sql
@@ -1,6 +1,6 @@
 /* !! Drop table only if it exists already */
-DROP TABLE pgWordFrequency;
-CREATE TABLE pgWordFrequency 
+DROP TABLE IF EXISTS pgWordFrequency;
+CREATE TABLE IF NOT EXISTS pgWordFrequency
 (word varchar, affix_rules char(7), frequency integer, freq_cat char(1), PRIMARY KEY(word));
 
 copy pgWordFrequency from 'C:\Documents and Settings\bcouriol\coding\Webstorm\JS_ME\assets\database\freq_table.csv'
@@ -13,4 +13,17 @@ CREATE INDEX freq_idx ON pgWordFrequency(frequency);
 UPDATE pgWordFrequency set freq_cat = '';
 UPDATE pgWordFrequency set freq_cat = 'A' where frequency > 12000;
 
+-- create table short (which is the same but a rank column instead of affix rules)
+DROP TABLE IF EXISTS pgwordfrequency_short;
+CREATE TABLE IF NOT EXISTS pgwordfrequency_short
+(
+  rank integer,
+  lemma character varying,
+  absolute_frequency integer,
+  freq_cat character(2)
+)
 
+COPY pgwordfrequency_short FROM 'C:\Documents and Settings\bcouriol\coding\Webstorm\Airlang\public\assets\pgwordfrequency_short.csv' DELIMITER ',' NULL AS ' ' CSV HEADER;
+
+-- maybe an index on lemma?
+CREATE INDEX lemma_btree_idx ON pgwordfrequency_short(lemma);
